e30ee5120c260125f1030c937be9808a8540e125
pengxiaoo <alexpengxiao@gmail.com>
2024-09-16 15:32:29 +0800

    init commit

---
+++ batch_task.py
@@ -0,0 +1,115 @@
+import os
+import time
+import datetime
+import json
+from dotenv import load_dotenv
+from openai import AzureOpenAI
+
+# refer to
+# https://learn.microsoft.com/en-us/azure/ai-services/openai/how-to/batch?tabs=standard-input&pivots=programming-language-python
+
+load_dotenv()
+# use constants defined in .env file
+client = AzureOpenAI(
+    api_key=os.getenv("API_KEY"),
+    api_version=os.getenv("API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+)
+
+
+def convert_csv_to_jsonl(csv_file_path: str, prompt: str) -> str:
+    """
+    :param csv_file_path: path of the input csv file
+    :param prompt: prompt used in chat/completions
+    :return: path of the generated jsonl file
+    """
+    # todo: copy and optimize your code from csv2json.py.
+    pass
+
+
+def upload_file(csv_file_path: str, prompt: str) -> str:
+    jsonl_file_path = convert_csv_to_jsonl(csv_file_path, prompt)
+    # Upload a file with a purpose of "batch"
+    file = client.files.create(
+        file=open(jsonl_file_path, "rb"),
+        purpose="batch"
+    )
+    return file.id
+
+
+def create_batch_job(file_id: str) -> str:
+    # Create a batch job
+    batch_response = client.batches.create(
+        input_file_id=file_id,
+        endpoint="/chat/completions",
+        completion_window="24h",
+    )
+    print(batch_response.model_dump_json(indent=2))
+    return batch_response.id
+
+
+def upload_and_create_job(csv_file_path: str, prompt: str) -> str:
+    file_id = upload_file(csv_file_path, prompt)
+    status = "pending"
+    while status != "processed":
+        time.sleep(15)
+        file_response = client.files.retrieve(file_id)
+        status = file_response.status
+        print(f"{datetime.datetime.now()} File Id: {file_id}, Status: {status}")
+    return create_batch_job(file_id)
+
+
+def track_and_save_job_result(batch_id: str) -> str:
+    batch_response = None
+    status = "validating"
+    output_file_id = None
+    result_file_path = None
+    while status not in ("completed", "failed", "canceled"):
+        time.sleep(60)
+        batch_response = client.batches.retrieve(batch_id)
+        status = batch_response.status
+        output_file_id = batch_response.output_file_id
+        print(f"{datetime.datetime.now()} Batch Id: {batch_id},  Status: {status}")
+
+    if status == "failed":
+        if batch_response:
+            for error in batch_response.errors.data:
+                print(f"Error code {error.code} Message {error.message}")
+    elif status == "completed" and output_file_id:
+        result_file_path = save_job_result(output_file_id)
+    else:
+        print("The batch job is canceled.")
+    return result_file_path
+
+
+def save_job_result(output_file_id) -> str:
+    file_response = client.files.content(output_file_id)
+    raw_responses = file_response.text.strip().split('\n')
+    result_file_path = "output_data/result.jsonl"  # or "output_data/result.csv", if you prefer
+    for raw_response in raw_responses:
+        json_response = json.loads(raw_response)
+        formatted_json = json.dumps(json_response, indent=2)
+        print(formatted_json)
+        # todo: save the result to a file in /output_data
+    return result_file_path
+
+
+def join_results_with_original_data(csv_file_path: str, result_file_path: str):
+    # todo: there should be a one-to-one mapping between the input csv file and the output result file so that we can match the results with the original data
+    pass
+
+
+if __name__ == "__main__":
+    # todo: use sample data first, after testing, we can change to full data
+    csv_file_path = "input_data/golf_course_reviews_sample.csv"
+    prompt_templates = [
+        """
+            You are a sentiment analysis model, please classify the sentiment of this text as either 'positive' or 'negative' 
+            or 'neutral'. Just answer one word:
+        """,
+    ]
+    for prompt_template in prompt_templates:
+        batch_id = upload_and_create_job(csv_file_path, prompt_template)
+        result_file_path = track_and_save_job_result(batch_id)
+        if result_file_path:
+            join_results_with_original_data(csv_file_path, result_file_path)
---
+++ readme.md
@@ -0,0 +1,8 @@
+### there are mainly 2 files:
+
+- batch_task.py for uploading source data and downloading the result
+- visualize.ipynb for visualizing the result
+
+### save credential constants in .env file
+
+### ignore the .env file, input_data folder and output_data folder in .gitignore
